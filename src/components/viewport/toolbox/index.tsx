08ff50a24df2ab49d624ac9b792e4365edb49f13
lovesmallpaes <mike.huang.mikank@gmail.com>
2025-03-31 14:27:41 +0100

    Refactored toolbox

--- src/components/viewport/toolbox/index.tsx
+++ src/components/viewport/toolbox/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './Toolbox.css';
 
 import ZoomInIcon from '../../../assets/svg/icons/zoom-in.svg';
@@ -26,61 +26,74 @@ export const Toolbox: React.FC<ToolboxProps> = ({
   isHistogramVisible,
   isDisabled = false,
 }) => {
+  const mainControls = useMemo(
+    () => [
+      {
+        onClick: onZoomIn,
+        icon: ZoomInIcon,
+        label: 'Zoom in',
+        title: 'Zoom in',
+      },
+      {
+        onClick: onZoomOut,
+        icon: ZoomOutIcon,
+        label: 'Zoom out',
+        title: 'Zoom out',
+      },
+      {
+        onClick: onCenter,
+        icon: CenterFocusIcon,
+        label: 'Center view',
+        title: 'Center view',
+      },
+      {
+        onClick: onReset,
+        icon: ResetIcon,
+        label: 'Reset view',
+        title: 'Reset view',
+      },
+    ],
+    [onZoomIn, onZoomOut, onCenter, onReset]
+  );
+
+  const histogramControl = useMemo(
+    () => ({
+      onClick: onToggleHistogram,
+      icon: BarChartIcon,
+      label: isHistogramVisible ? 'Hide histogram' : 'Show histogram',
+      title: isHistogramVisible ? 'Hide histogram' : 'Show histogram',
+    }),
+    [onToggleHistogram, isHistogramVisible]
+  );
+
   return (
     <div className={`toolbox ${isDisabled ? 'toolbox--disabled' : ''}`}>
       <div className="toolbox__controls">
-        <button
-          onClick={onZoomIn}
-          className="toolbox__button"
-          disabled={isDisabled}
-          aria-label="Zoom in"
-          title="Zoom in"
-          type="button"
-        >
-          <ZoomInIcon />
-        </button>
-        <button
-          onClick={onZoomOut}
-          className="toolbox__button"
-          disabled={isDisabled}
-          aria-label="Zoom out"
-          title="Zoom out"
-          type="button"
-        >
-          <ZoomOutIcon />
-        </button>
-        <button
-          onClick={onCenter}
-          className="toolbox__button"
-          disabled={isDisabled}
-          aria-label="Center view"
-          title="Center view"
-          type="button"
-        >
-          <CenterFocusIcon />
-        </button>
-        <button
-          onClick={onReset}
-          className="toolbox__button"
-          disabled={isDisabled}
-          aria-label="Reset view"
-          title="Reset view"
-          type="button"
-        >
-          <ResetIcon />
-        </button>
+        {mainControls.map((control, index) => (
+          <button
+            key={index}
+            onClick={control.onClick}
+            className="toolbox__button"
+            disabled={isDisabled}
+            aria-label={control.label}
+            title={control.title}
+            type="button"
+          >
+            <control.icon />
+          </button>
+        ))}
       </div>
       <hr className="toolbox__divider" />
       <div className="toolbox__controls">
         <button
-          onClick={onToggleHistogram}
-          className={`toolbox__button`}
+          onClick={histogramControl.onClick}
+          className="toolbox__button"
           disabled={isDisabled}
-          aria-label={isHistogramVisible ? 'Hide histogram' : 'Show histogram'}
-          title={isHistogramVisible ? 'Hide histogram' : 'Show histogram'}
+          aria-label={histogramControl.label}
+          title={histogramControl.title}
           type="button"
         >
-          <BarChartIcon />
+          <histogramControl.icon />
         </button>
       </div>
     </div>
