5eaf5b187c993bd30ac2d71181ffd9ab5bdf2bcf
lovesmallpaes <mike.huang.mikank@gmail.com>
2025-03-31 13:51:26 +0100

    Removed unused packages

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-import { type PixiReactElementProps } from '@pixi/react';
-import { type Viewport } from 'pixi-viewport';
-
-declare module '@pixi/react' {
-  interface PixiElements {
-    viewport: PixiReactElementProps<typeof Viewport>;
-  }
-}
